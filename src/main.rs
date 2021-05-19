75ad9a7cbc957eb9c8f0e30a8e703e2221ac7f85
Heru Handika <herubiolog@gmail.com>
2021-05-19 15:05:08 -0500

    Refactor the code and add test.

--- src/main.rs
+++ src/main.rs
@@ -23,24 +23,45 @@ fn find_contigs(path: &str) {
             let fname = String::from(e.path().to_string_lossy());
             if fname.ends_with("/contigs.fasta") {
                 let path = PathBuf::from(fname);
-
-                // Get ID from path to the contigs. Assuming the first index is the species ID
-                let dirs: Vec<_> = path.components().map(|d| d.as_os_str()).collect();
-                assert!(dirs.len() > 1, "INVALID FOLDER STRUCTURE TO USE AUTO");
-                let id = String::from(dirs[1].to_string_lossy());
-                let sym_name = format!("{}_contigs.fasta", id);
-                let contig_sym = symdir.join(sym_name);
-                create_symlink(&path, &contig_sym);
+                create_symlink(&path, &symdir);
             }
         });
 }
 
-fn create_symlink(path: &Path, contig_sym: &Path) {
+fn create_symlink(path: &Path, symdir: &Path) {
+    // Use folder name as contig name.
+    let fname = get_fname(path);
+    let contig_sym = symdir.join(fname);
     let sympath = path.canonicalize().expect("CAN'T CANOCALIZE PATH");
-    unix::fs::symlink(&sympath, contig_sym).expect("CAN'T CREATE SYMLINK");
+
+    unix::fs::symlink(&sympath, &contig_sym).expect("CAN'T CREATE SYMLINK");
+    
     println!(
         "{} \x1b[0;36m => \x1b[0m {}",
         sympath.to_string_lossy(),
         contig_sym.to_string_lossy()
     );
 }
+
+fn get_fname(path: &Path) -> PathBuf {
+    let dirs: Vec<_> = path.components().map(|d| d.as_os_str()).collect();
+    assert!(dirs.len() > 1, "INVALID FOLDER STRUCTURE");
+    let mut fname = PathBuf::from(String::from(dirs[1].to_string_lossy()));
+    fname.set_extension(path.extension().unwrap());
+    
+    fname
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn output_fname_test(){
+        let path = Path::new("./Species_epithet_ABCD123425/contigs.fasta");
+        let res = PathBuf::from("Species_epithet_ABCD123425.fasta");
+
+        assert_eq!(res, get_fname(path));
+    }
+}
+
